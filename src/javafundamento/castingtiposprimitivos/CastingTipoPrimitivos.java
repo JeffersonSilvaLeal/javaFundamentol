2117afcc4d31e85f07d0c8a838c63180a1f5d7cc
JeffersonSilva <drankhar@gmail.com>
2022-01-10 23:42:30 -0300

    Casting implicito e Explicíto

---
+++ src/javafundamento/castingtiposprimitivos/CastingTipoPrimitivos.java
@@ -0,0 +1,35 @@
+package javafundamento.castingtiposprimitivos;
+
+public class CastingTipoPrimitivos {
+
+	public static void main(String[] args) {
+	
+		
+		/**
+		 * Casting alterar um tipo de dado
+		 * 
+		 * byte => int => float => long => double
+		 */
+		
+		/*
+		 * Casting implicito Quando o valor atribuito cabe dentro do tipo de dado
+		 */
+		
+		long x = 10;
+		double d = 35;
+		
+		System.out.println("Long: " + x);
+		System.out.println("Double: " + d);
+		
+		/*
+		 * Casting explicito Quando o valor do atributo não cabe dentro do tipo de dado
+		 */
+		
+		float g = 40.0f;
+		float f = (float)40.0;
+		
+		System.out.println("Float: " + g);
+		System.out.println("Float: "+ f);
+	}
+
+}
